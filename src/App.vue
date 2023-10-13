7933274ec50fc31614139c4d24360b673547da90
Ansfati <23wm1329@student.gs.chiba-u.jp>
2023-10-13 16:49:36 +0900

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <Header :username='name'></Header>
+    <Body @add='add1'></Body>
+    <Body @add='add2'></Body>
+    <p>total : {{totalcount}}</p>
+  </div>
+</template>
+
+<script>
+import Header from './components/HeaderComponent.vue'
+import Body from './components/BodyComponent.vue'
+
+export default {
+   data(){
+      return{
+         name:'kiyokiyo',
+         count1: 0,
+         count2: 0,
+         totalcount: 0
+      }
+   },
+
+   components: {
+     Header,
+     Body
+   },
+   
+   methods:{
+      add1(count){
+         this.count1 = count;
+         this.totalcount = this.count1 + this.count2;
+      },
+      add2(count){
+         this.count2 = count;
+         this.totalcount = this.count1 + this.count2;
+      }
+   }
+}
+</script>
+
