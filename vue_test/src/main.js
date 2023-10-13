7db17774c01c6eb83d13dfeb4b7f040b5ed0ed11
Ansfati <23wm1329@student.gs.chiba-u.jp>
2023-10-13 13:47:58 +0900

    1013

--- vue_test/src/main.js
+++ vue_test/src/main.js
@@ -1,4 +1,19 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import ElmentPlus from 'element-plus'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import router from './router'
 import App from './App.vue'
+import './assets/main.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)){
+	app.component(key, component)
+}
+
+const pinia = createPinia()
+app.use(pinia)
+app.use(router)
+app.use(ElementPlus).mount('#app')
+
+export default app;
